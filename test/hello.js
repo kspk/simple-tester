1143f58311895d3bca0890417383ef119ced5f9a
Abhishek Kumar <ak@Abhisheks-MacBook-Pro.local>
2020-03-01 14:45:32 -0800

    Update with documentation comments and fix test run state.

--- test/hello.js
+++ test/hello.js
@@ -1,6 +1,17 @@
-import { TestCase, TestException } from '../src/TestCase.js';
-import { TestRunner } from '../src/TestRunner.js';
+/**
+ * @file hello.js
+ * Provides a rudimentry test for the simple-tester. 
+ */
 
+import { TestCase, TestException } from '../src/testcase.js';
+import { TestRunner } from '../src/testrunner.js';
+
+/**
+ * @function hello A test function for simple-tester. 
+ * Allows testing of a common flow and an exception path.
+ * 
+ * @param {string} name any random string value
+ */
 let hello = (name) => {
     if(!name) {
         throw "Argument exception";
@@ -10,10 +21,28 @@ let hello = (name) => {
     }
 }
 
+/** @constant POSITIVE_TESTS Count of positive tests for simple-tester. */
+let POSITIVE_TESTS = 2;
+/** @constant NEGATIVE_TESTS Count of negative tests for simple-tester. */
+let NEGATIVE_TESTS = 1;
+
+/** @constant cases List of test cases for simple-tester. */
 let cases = [
+    /** Positive tests */
     new TestException(null, "Argument exception"),
-    new TestCase('', "Hello!"),
-    new TestCase("World", "Hello World!")
+    new TestCase("World", "Hello World!"),
+
+    /** Negative tests */
+    new TestCase('', "Hello!")
 ];
 
-process.exit((new TestRunner(cases, hello)).run());
+/**
+ * Test the tester. 
+ * 
+ * There are intentionally 2 passing and 1 failing tests 
+ * included in the test run above. 
+ * 
+ * Check we have the correct count.
+ */
+let result = (new TestRunner(cases, hello)).run();
+process.exit(result == POSITIVE_TESTS - NEGATIVE_TESTS ? 0 : result);
